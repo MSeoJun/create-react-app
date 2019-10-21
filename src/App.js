b81254186c114ef84933fc19c945975e34cf76d2
MSeoJun <mun1582w@naver.com>
2019-10-21 12:07:05 +0900

    6장

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
-import Say from './Say';
-import Counter from './Counter';
-import Event from './EventPractice';
-import ValidationSample from './ValidationSample';
+// import ScrollBox from './ScrollBox';
+// import Say from './Say';
+// import Counter from './Counter';
+// import Event from './EventPractice';
+// import ValidationSample from './ValidationSample';
+import IterationSample from './IterationSample';
+
 class App extends Component {
   render() {
     return (
-    <center>
-      <div>
-        <Say/>
-        <Counter/>
-        <Event/> <br/>
-        <ValidationSample/> <br/>
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    </center>
+        <IterationSample/>
+        // {/* <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        // <button onClick={() => this.scrollBox.scrollToBottom()}>
+        //   맨 밑으로
+        // </button> */}
     );
   }
 }
+
 export default App;
